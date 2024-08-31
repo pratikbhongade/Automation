9e22f557c9a6fbc781f46bc9625f5638b616d3a8
Pratik Bhongade <pratikbhongade7@gmail.com>
2024-08-31 18:04:17 +0530

    Update email_sender.py

--- email_sender.py
+++ email_sender.py
@@ -2,12 +2,12 @@ import os
 import pythoncom
 import win32com.client as win32
 
-def send_email(subject, validation_results, log_file_path, environment):
+def send_email(subject, validation_results, success, log_file_path):
     email_body = (
         "<html>"
         "<body style='font-family: Arial, sans-serif;'>"
-        f"<p>Hi Team,</p>"
-        f"<p>Please find the validation result of <strong>FPA {environment.upper()} Environment Validation</strong>:</p>"
+        "<p>Hi Team,</p>"
+        "<p>Please find the validation result of <strong>FPA IT Application</strong>:</p>"
         "<pre style='font-size: 14px; color: #333;'>"
     )
 
@@ -15,7 +15,7 @@ def send_email(subject, validation_results, log_file_path, environment):
         email_body += f"{result}\n"
 
     email_body += "</pre>"
-    if all([status == "Success" for result, status in validation_results]):
+    if success:
         email_body += "<p style='font-size: 18px; color: green;'><strong>Validation Successful</strong></p>"
     else:
         email_body += "<p style='font-size: 18px; color: red;'><strong>Validation Failed</strong></p>"
@@ -30,11 +30,21 @@ def send_email(subject, validation_results, log_file_path, environment):
     )
 
     pythoncom.CoInitialize()
-    outlook = win32.Dispatch('outlook.application')
-    mail = outlook.CreateItem(0)
-    mail.To = 'Pratik_Bhongade@keybank.com'  # Replace with the recipient email addresses
-    mail.Subject = subject
-    mail.HTMLBody = email_body
-    mail.Attachments.Add(log_file_path)  # Attach the log file
-    mail.Send()
-    pythoncom.CoUninitialize()
+    try:
+        outlook = win32.Dispatch('outlook.application')
+        mail = outlook.CreateItem(0)
+        mail.To = 'Pratik_Bhongade@keybank.com'  # Replace with the recipient email addresses
+        mail.Subject = subject
+        mail.HTMLBody = email_body
+
+        # Check if the log file exists before attaching it
+        if os.path.exists(log_file_path):
+            mail.Attachments.Add(log_file_path)  # Attach the log file
+        else:
+            print(f"Log file not found: {log_file_path}")
+
+        mail.Send()
+    except Exception as e:
+        print(f"Failed to send email: {e}")
+    finally:
+        pythoncom.CoUninitialize()
